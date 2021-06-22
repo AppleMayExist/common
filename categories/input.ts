ae0a025087b91a5daa4f43069049d78d8fb5aa15
AppleMayExist <someweirdrandomapple@protonmail.com>
2021-06-22 09:18:27 +0000

    split input to improve ts type checking

--- categories/input.ts
+++ categories/input.ts
@@ -1,15 +1,30 @@
 /**
  * Generator function that outputs keystrokes.
- * @param returnChar States whether to return a number (corresponding to ASCII characted), or just the character (from a TextDecoder).
  * @param disableSetRaw States whether to disable setRaw in stdin when finished. True by default.
  */
-export const input = async function*(returnChar = false, disableSetRaw = true) {
+export const input = async function*(disableSetRaw = true) {
     Deno.setRaw(0, true);
     const x = new Uint8Array(1);
     while (true) {
         await Deno.stdin.read(x);
         if ([0, 10, 13].includes(x[0])) {break;}
-        else {yield returnChar ? new TextDecoder().decode(x) : x[0];}
+        else {yield new TextDecoder().decode(x);}
+    }
+    disableSetRaw && Deno.setRaw(0, false);
+    return;
+};
+
+/**
+ * Generator function that outputs keystrokes' ascii code.
+ * @param disableSetRaw States whether to disable setRaw in stdin when finished. True by default.
+ */
+ export const inputRaw = async function*(disableSetRaw = true) {
+    Deno.setRaw(0, true);
+    const x = new Uint8Array(1);
+    while (true) {
+        await Deno.stdin.read(x);
+        if ([0, 10, 13].includes(x[0])) {break;}
+        else {yield x[0];}
     }
     disableSetRaw && Deno.setRaw(0, false);
     return;
