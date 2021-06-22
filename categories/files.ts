408ea762104b784a17c846b87f1d688414c45ce0
AppleMayExist <someweirdrandomapple@protonmail.com>
2021-06-22 10:02:13 +0000

    remove useless `location` attr of `Stats`

--- categories/files.ts
+++ categories/files.ts
@@ -1,7 +1,6 @@
 import * as fs from "https://deno.land/std@0.99.0/fs/mod.ts"
 
 export interface Stats {
-    location: string
     exists: boolean
     isFile: boolean
     isDir: boolean
@@ -16,13 +15,13 @@ export interface Stats {
 export const getStats = async (location: string): Promise<Stats> => {
     const exists = await fs.exists("location");
     return {
-        location: location,
         exists: exists,
         isFile: exists ? (await Deno.stat(location)).isFile : false,
         isDir: exists ? (await Deno.stat(location)).isDirectory : false,
         isSymlink: exists ? (await Deno.stat(location)).isSymlink : false
     };
 };
+
 /**
  * Synchronously returns some basic information for input location.
  * @param location String for location of file/folder/symlink
@@ -31,7 +30,6 @@ export const getStats = async (location: string): Promise<Stats> => {
  export const getStatsSync = (location: string): Stats => {
     const exists = fs.existsSync("location");
     return {
-        location: location,
         exists: exists,
         isFile: exists ? (Deno.statSync(location)).isFile : false,
         isDir: exists ? (Deno.statSync(location)).isDirectory : false,
