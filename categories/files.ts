c6c1e41d62e1775f9a5040a1caa165d5048b2a75
AppleMayExist <someweirdrandomapple@protonmail.com>
2021-06-22 10:00:25 +0000

    add sync `getStats` function

--- categories/files.ts
+++ categories/files.ts
@@ -23,3 +23,18 @@ export const getStats = async (location: string): Promise<Stats> => {
         isSymlink: exists ? (await Deno.stat(location)).isSymlink : false
     };
 };
+/**
+ * Synchronously returns some basic information for input location.
+ * @param location String for location of file/folder/symlink
+ * @returns Basic information of input location
+ */
+ export const getStatsSync = (location: string): Stats => {
+    const exists = fs.existsSync("location");
+    return {
+        location: location,
+        exists: exists,
+        isFile: exists ? (Deno.statSync(location)).isFile : false,
+        isDir: exists ? (Deno.statSync(location)).isDirectory : false,
+        isSymlink: exists ? (Deno.statSync(location)).isSymlink : false
+    };
+};
